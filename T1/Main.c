cdfa5ecc050b5418257219b8ec8b44b209590e32
Filipe M. Almeida <filipemedeirosalmeida@gmail.com>
2024-08-24 22:17:46 -0300

    Início da leitura de arquivos

--- T1/Main.c
+++ T1/Main.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 #include "readFile.h"
 
-int main(int argc, char *argv[]){
-    //char nomeLog[20];
-        //Recebendo nome lógico 
-    if (argc>1)
-    {
-        readFile(argv[1]);
-        return 0;
+int readFile(char nomeLog[20]) {
+    FILE *file;
+    int id;
+    char nome_logico[20];
+    char nome_fisico[20];
+    
+    file = fopen("table.dic", "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo table.dic\n");
+        return -1;
     }
-    printf("Missing parameter: logical file name\n");
-    return 0;
+
+    while (fscanf(file, "%d %20s %20s", &id, nome_logico, nome_fisico) != EOF) {
+        if (strncmp(nomeLog, nome_logico, 20) == 0) {
+            printf("Nome lógico encontrado: %s\n", nome_logico);
+            printf("Nome físico correspondente: %s\n", nome_fisico);
+
+            FILE *f = fopen(nome_fisico, "r");
+            if (f == NULL) {
+                printf("Erro ao abrir o arquivo %s\n", nome_fisico);
+                fclose(file);
+                return -1;
+            }
+
+            printf("Arquivo %s aberto com sucesso.\n", nome_fisico);
+            fclose(f);
+            
+            FILE *attFile = fopen("att.dic", "r");
+            if (attFile == NULL) {
+                printf("Erro ao abrir o arquivo att.dic\n");
+                fclose(file);
+                return -1;
+            }
+            
+            atributo att;
+            while (fscanf(attFile, "%d %20s %c %c %d", &att.id_arquivo_att, att.nome_att, &att.tipo, &att.opcional, &att.tamanho) != EOF) {
+                if (att.id_arquivo_att == id) {
+                    printf("Atributo: %s, Tipo: %c, Opcional: %c, Tamanho: %d\n", att.nome_att, att.tipo, att.opcional, att.tamanho);
+                }
+            }
+
+            fclose(attFile);
+            fclose(file);
+            return 0;
+        }
+    }
+
+    printf("Nome lógico %s não encontrado em table.dic\n", nomeLog);
+    fclose(file);
+    return -1;
 }
--- T1/readFile.h
+++ T1/readFile.h
@@ -1,10 +1,15 @@
+#ifndef READFILE_H
+#define READFILE_H
 
-struct att{
+struct att {
     int id_arquivo_att;
     char nome_att[20];
     char tipo;
     char opcional;
     int tamanho;
-}; typedef struct att atributo;
+}; 
+typedef struct att atributo;
 
 int readFile(char nomeLog[20]);
+
+#endif
